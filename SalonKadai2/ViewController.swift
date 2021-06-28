d5e1d27dc05944cb74abf2f905ed5365e2cbfd23
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-06-28 10:17:45 +0900

    計算処理追加しました

--- SalonKadai2/ViewController.swift
+++ SalonKadai2/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet private weak var firstNumberTextField: UITextField!
+    @IBOutlet private weak var secondNumberTextField: UITextField!
+    @IBOutlet private weak var fourArithmeticOperationsSecmentedControl: UISegmentedControl!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    private let notificationCenter = NotificationCenter()
+    private var viewModel: ViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstNumberTextField.keyboardType = .numberPad
+        secondNumberTextField.keyboardType = .numberPad
+        viewModel = ViewModel(notificationCenter: notificationCenter)
+        
+        firstNumberTextField.addTarget(self,
+                                       action: #selector(textFieldEditingDidEnd),
+                                       for: .editingChanged)
+        secondNumberTextField.addTarget(self,
+                                       action: #selector(textFieldEditingDidEnd),
+                                       for: .editingChanged)
+        fourArithmeticOperationsSecmentedControl.addTarget(self,
+                                       action: #selector(textFieldEditingDidEnd),
+                                       for: .valueChanged)
+
+        notificationCenter.addObserver(self,
+                                       selector: #selector(updateResultLabel),
+                                       name: viewModel?.changeResultLabel,
+                                       object: nil)
+        
     }
+}
 
-
+extension ViewController {
+    @objc func textFieldEditingDidEnd(sender: UITextField) {
+        let index = fourArithmeticOperationsSecmentedControl.selectedSegmentIndex
+        viewModel?.numberInputed(firstNumber: firstNumberTextField.text, secondNumber: secondNumberTextField.text, index: index)
+        
+    }
+    
+    @objc func updateResultLabel(notification: Notification) {
+        guard let text = notification.object as? String else { return }
+        resultLabel.text = text
+    }
 }
 
---
+++ SalonKadai2/ViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  ViewModel.swift
+//  SalonKadai2
+//
+//  Created by 坂本龍哉 on 2021/06/26.
+//
+
+import Foundation
+
+
+final class ViewModel {
+    let changeResultLabel = Notification.Name("changeResultLabel")
+    
+    private let notificationCenter: NotificationCenter
+    private let model: ModelProtocol
+    
+    init(notificationCenter: NotificationCenter, model: ModelProtocol = Model()) {
+        self.notificationCenter = notificationCenter
+        self.model = model
+    }
+    
+    func numberInputed(firstNumber: String?, secondNumber: String?, index: Int) {
+        var state: CalculateState = .plus
+        switch index {
+        case 0: state = .plus
+        case 1: state = .minus
+        case 2: state = .multipliedBy
+        case 3: state = .dividedBy
+        default: break
+        }
+
+        let result = model.calculate(firstNumber: firstNumber, secondNumber: secondNumber, calculateStete: state)
+        
+        switch result {
+        case .success(let result):
+            notificationCenter.post(name: changeResultLabel, object: result)
+        case .failure(let error as ModelError):
+            notificationCenter.post(name: changeResultLabel, object: error.errorText)
+        case _: fatalError("予想外")
+        }
+    }
+}
+
+extension ModelError {
+    var errorText: String {
+        switch self {
+        case .invalidBothNumbers:
+            return "未入力が２箇所あります"
+        case .invalidFirstNumber:
+            return "2つ目が未入力です"
+        case .invalidSecondNumber:
+            return "1つ目が未入力です"
+        case .invalidDividedByZero:
+            return "割る数には０以外の数字を入力してください"
+        }
+    }
+}
